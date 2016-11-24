9a2a51b0f5c95d8d4cb1e7f21ae245fd874e992f
Alex C. Donnelly <alexandercassius@hotmail.com>
2016-11-24 13:50:58 -0500

    Added View
    Added Customer Info View

---
+++ NerdBlock/Views/CustomerInfo.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace NerdBlock.Views
+{
+    partial class CustomerInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpInfo = new System.Windows.Forms.GroupBox();
+            this.grpSubscriptions = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.lstPastSubs = new System.Windows.Forms.ListBox();
+            this.lstActiveSubs = new System.Windows.Forms.ListBox();
+            this.grpInfo.SuspendLayout();
+            this.grpSubscriptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpInfo
+            // 
+            this.grpInfo.Controls.Add(this.label17);
+            this.grpInfo.Controls.Add(this.label11);
+            this.grpInfo.Controls.Add(this.label16);
+            this.grpInfo.Controls.Add(this.label15);
+            this.grpInfo.Controls.Add(this.label14);
+            this.grpInfo.Controls.Add(this.label13);
+            this.grpInfo.Controls.Add(this.label12);
+            this.grpInfo.Controls.Add(this.lblID);
+            this.grpInfo.Controls.Add(this.label9);
+            this.grpInfo.Controls.Add(this.label7);
+            this.grpInfo.Controls.Add(this.label8);
+            this.grpInfo.Controls.Add(this.label6);
+            this.grpInfo.Controls.Add(this.label4);
+            this.grpInfo.Controls.Add(this.label5);
+            this.grpInfo.Controls.Add(this.label3);
+            this.grpInfo.Controls.Add(this.label2);
+            this.grpInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpInfo.Location = new System.Drawing.Point(3, 3);
+            this.grpInfo.Name = "grpInfo";
+            this.grpInfo.Size = new System.Drawing.Size(831, 194);
+            this.grpInfo.TabIndex = 0;
+            this.grpInfo.TabStop = false;
+            this.grpInfo.Text = "Customer Information";
+            this.grpInfo.Enter += new System.EventHandler(this.grpInfo_Enter);
+            // 
+            // grpSubscriptions
+            // 
+            this.grpSubscriptions.Controls.Add(this.lstActiveSubs);
+            this.grpSubscriptions.Controls.Add(this.lstPastSubs);
+            this.grpSubscriptions.Controls.Add(this.label10);
+            this.grpSubscriptions.Controls.Add(this.label1);
+            this.grpSubscriptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpSubscriptions.Location = new System.Drawing.Point(3, 203);
+            this.grpSubscriptions.Name = "grpSubscriptions";
+            this.grpSubscriptions.Size = new System.Drawing.Size(831, 344);
+            this.grpSubscriptions.TabIndex = 1;
+            this.grpSubscriptions.TabStop = false;
+            this.grpSubscriptions.Text = "Subscriptions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(114, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(474, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Joined";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(71, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "User Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(71, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "First Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(430, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Last Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(411, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 20);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Home Address";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(392, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(140, 20);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Shipping Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Active Subscriptions";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(546, 51);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(150, 20);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Past Subscriptions";
+            // 
+            // lblID
+            // 
+            this.lblID.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(171, 82);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(88, 20);
+            this.lblID.TabIndex = 3;
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(171, 115);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(204, 20);
+            this.label12.TabIndex = 9;
+            // 
+            // label13
+            // 
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(171, 150);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(204, 20);
+            this.label13.TabIndex = 10;
+            // 
+            // label14
+            // 
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(171, 46);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(204, 20);
+            this.label14.TabIndex = 11;
+            // 
+            // label15
+            // 
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(541, 45);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(236, 20);
+            this.label15.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(541, 150);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(236, 20);
+            this.label11.TabIndex = 15;
+            // 
+            // label16
+            // 
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(541, 115);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(236, 20);
+            this.label16.TabIndex = 14;
+            // 
+            // label17
+            // 
+            this.label17.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(541, 81);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(236, 20);
+            this.label17.TabIndex = 16;
+            // 
+            // lstPastSubs
+            // 
+            this.lstPastSubs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPastSubs.FormattingEnabled = true;
+            this.lstPastSubs.ItemHeight = 20;
+            this.lstPastSubs.Location = new System.Drawing.Point(415, 87);
+            this.lstPastSubs.Name = "lstPastSubs";
+            this.lstPastSubs.Size = new System.Drawing.Size(410, 244);
+            this.lstPastSubs.TabIndex = 3;
+            // 
+            // lstActiveSubs
+            // 
+            this.lstActiveSubs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstActiveSubs.FormattingEnabled = true;
+            this.lstActiveSubs.ItemHeight = 20;
+            this.lstActiveSubs.Location = new System.Drawing.Point(6, 87);
+            this.lstActiveSubs.Name = "lstActiveSubs";
+            this.lstActiveSubs.Size = new System.Drawing.Size(403, 244);
+            this.lstActiveSubs.TabIndex = 4;
+            // 
+            // CustomerInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grpSubscriptions);
+            this.Controls.Add(this.grpInfo);
+            this.Name = "CustomerInfo";
+            this.Size = new System.Drawing.Size(842, 550);
+            this.grpInfo.ResumeLayout(false);
+            this.grpInfo.PerformLayout();
+            this.grpSubscriptions.ResumeLayout(false);
+            this.grpSubscriptions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpInfo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpSubscriptions;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ListBox lstActiveSubs;
+        private System.Windows.Forms.ListBox lstPastSubs;
+    }
+}
---
+++ NerdBlock/Views/CustomerInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/// <summary>
+/// This page might be redesigned to add a 'Subscriptions' button that opens a form with all the subscriptions pane information, instead of having
+/// it on the same form.
+/// </summary>
+namespace NerdBlock.Views
+{
+    public partial class CustomerInfo : UserControl
+    {
+        public CustomerInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void grpInfo_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
