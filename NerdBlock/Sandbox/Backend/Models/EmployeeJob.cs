07ac2a1a2bfc6fec1fde0ce407fc9d6b66abdc81
Ryan Schuette <Ryan_As@live.com>
2016-11-27 15:48:25 -0500

    SQL update and Models started
    enough said

---
+++ NerdBlock/Sandbox/Backend/Models/EmployeeJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdBlock.Sandbox.Backend.Models
+{
+    [DataModel("tblemployeejob")]
+    class EmployeeJob
+    {
+        [DataField("employeeid", QueryParamType.Integer), PrimaryKey, ForeignKey("tblemployee", "employeeid")]
+        public int? EmployeeId { get; set; }
+
+        [DataField("roleid", QueryParamType.Integer), PrimaryKey, ForeignKey("tblemployeerole", "roleid")]
+        public int? RoleId { get; set; }
+    }
+}
---
+++ NerdBlock/Sandbox/Backend/Models/PaymentInstance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdBlock.Sandbox.Backend.Models
+{
+    [DataModel("tblpaymentinstance")]
+    class PaymentInstance
+    {
+        [DataField("subscriptionid", QueryParamType.Integer), PrimaryKey, ForeignKey("tblsubscriptionid", "subscriptionid")]
+        public int? SubscriptionId { get; set; }
+
+        [DataField("paymentdate", QueryParamType.Date), PrimaryKey]
+        public DateTime? PaymentDate { get; set; }
+
+        [DataField("billinginfoid", QueryParamType.Integer), ForeignKey("tblbillinginfo", "billinginfoid")]
+        public int? BillingInfoId { get; set; }
+
+        [DataField("ispayed", QueryParamType.Boolean)]
+        public bool? IsPayed { get; set; }
+    }
+}
