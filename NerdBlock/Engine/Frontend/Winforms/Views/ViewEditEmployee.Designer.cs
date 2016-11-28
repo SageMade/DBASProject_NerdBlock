2444a917e86c8eeeedbc949a2527f780b7709677
Daniel Munusami <daniel.munusami@gmail.com>
2016-11-28 17:29:25 -0500

    Fixed ViewEditEmployee and ViewEditOrder

--- NerdBlock/Engine/Frontend/Winforms/Views/ViewEditEmployee.Designer.cs
+++ NerdBlock/Engine/Frontend/Winforms/Views/ViewEditEmployee.Designer.cs
@@ -31,32 +31,33 @@
             this.btnEdit = new System.Windows.Forms.Button();
             this.txtPostalCode = new System.Windows.Forms.TextBox();
             this.txtProvince = new System.Windows.Forms.TextBox();
-            this.txtPartFullTime = new System.Windows.Forms.TextBox();
-            this.txtAddress2 = new System.Windows.Forms.TextBox();
-            this.txtAddress1 = new System.Windows.Forms.TextBox();
-            this.txtCell = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
             this.txtPhone = new System.Windows.Forms.TextBox();
             this.txtPosition = new System.Windows.Forms.TextBox();
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.txtFirstName = new System.Windows.Forms.TextBox();
-            this.txtID = new System.Windows.Forms.TextBox();
             this.lblPostalCode = new System.Windows.Forms.Label();
             this.lblProvince = new System.Windows.Forms.Label();
-            this.lblPartFullTime = new System.Windows.Forms.Label();
-            this.lblAddress2 = new System.Windows.Forms.Label();
             this.lblAddress1 = new System.Windows.Forms.Label();
-            this.lblCell = new System.Windows.Forms.Label();
             this.lblPhone = new System.Windows.Forms.Label();
             this.lblPosition = new System.Windows.Forms.Label();
             this.lblLastName = new System.Windows.Forms.Label();
             this.lblFirstName = new System.Windows.Forms.Label();
             this.lblID = new System.Windows.Forms.Label();
             this.btnTerminate = new System.Windows.Forms.Button();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbRole = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtSIN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(147, 191);
+            this.btnEdit.Location = new System.Drawing.Point(145, 256);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(75, 23);
             this.btnEdit.TabIndex = 45;
@@ -65,85 +66,57 @@
             // 
             // txtPostalCode
             // 
-            this.txtPostalCode.Location = new System.Drawing.Point(309, 131);
+            this.txtPostalCode.Location = new System.Drawing.Point(293, 150);
             this.txtPostalCode.Name = "txtPostalCode";
             this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
             this.txtPostalCode.TabIndex = 44;
             // 
             // txtProvince
             // 
-            this.txtProvince.Location = new System.Drawing.Point(309, 105);
+            this.txtProvince.Location = new System.Drawing.Point(293, 95);
             this.txtProvince.Name = "txtProvince";
             this.txtProvince.Size = new System.Drawing.Size(100, 20);
             this.txtProvince.TabIndex = 43;
             // 
-            // txtPartFullTime
+            // txtAddress
             // 
-            this.txtPartFullTime.Location = new System.Drawing.Point(309, 76);
-            this.txtPartFullTime.Name = "txtPartFullTime";
-            this.txtPartFullTime.Size = new System.Drawing.Size(100, 20);
-            this.txtPartFullTime.TabIndex = 42;
-            // 
-            // txtAddress2
-            // 
-            this.txtAddress2.Location = new System.Drawing.Point(309, 50);
-            this.txtAddress2.Name = "txtAddress2";
-            this.txtAddress2.Size = new System.Drawing.Size(100, 20);
-            this.txtAddress2.TabIndex = 41;
-            // 
-            // txtAddress1
-            // 
-            this.txtAddress1.Location = new System.Drawing.Point(309, 24);
-            this.txtAddress1.Name = "txtAddress1";
-            this.txtAddress1.Size = new System.Drawing.Size(100, 20);
-            this.txtAddress1.TabIndex = 40;
-            // 
-            // txtCell
-            // 
-            this.txtCell.Location = new System.Drawing.Point(69, 154);
-            this.txtCell.Name = "txtCell";
-            this.txtCell.Size = new System.Drawing.Size(100, 20);
-            this.txtCell.TabIndex = 39;
+            this.txtAddress.Location = new System.Drawing.Point(293, 121);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 40;
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(69, 128);
+            this.txtPhone.Location = new System.Drawing.Point(70, 205);
             this.txtPhone.Name = "txtPhone";
             this.txtPhone.Size = new System.Drawing.Size(100, 20);
             this.txtPhone.TabIndex = 38;
             // 
             // txtPosition
             // 
-            this.txtPosition.Location = new System.Drawing.Point(69, 102);
+            this.txtPosition.Location = new System.Drawing.Point(70, 179);
             this.txtPosition.Name = "txtPosition";
             this.txtPosition.Size = new System.Drawing.Size(100, 20);
             this.txtPosition.TabIndex = 37;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(69, 76);
+            this.txtLastName.Location = new System.Drawing.Point(70, 153);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(100, 20);
             this.txtLastName.TabIndex = 36;
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(69, 50);
+            this.txtFirstName.Location = new System.Drawing.Point(70, 127);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(100, 20);
             this.txtFirstName.TabIndex = 35;
             // 
-            // txtID
-            // 
-            this.txtID.Location = new System.Drawing.Point(69, 24);
-            this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(100, 20);
-            this.txtID.TabIndex = 34;
-            // 
             // lblPostalCode
             // 
             this.lblPostalCode.AutoSize = true;
-            this.lblPostalCode.Location = new System.Drawing.Point(239, 134);
+            this.lblPostalCode.Location = new System.Drawing.Point(223, 153);
             this.lblPostalCode.Name = "lblPostalCode";
             this.lblPostalCode.Size = new System.Drawing.Size(64, 13);
             this.lblPostalCode.TabIndex = 33;
@@ -152,52 +125,25 @@
             // lblProvince
             // 
             this.lblProvince.AutoSize = true;
-            this.lblProvince.Location = new System.Drawing.Point(254, 108);
+            this.lblProvince.Location = new System.Drawing.Point(238, 98);
             this.lblProvince.Name = "lblProvince";
             this.lblProvince.Size = new System.Drawing.Size(49, 13);
             this.lblProvince.TabIndex = 32;
             this.lblProvince.Text = "Province";
             // 
-            // lblPartFullTime
-            // 
-            this.lblPartFullTime.AutoSize = true;
-            this.lblPartFullTime.Location = new System.Drawing.Point(204, 79);
-            this.lblPartFullTime.Name = "lblPartFullTime";
-            this.lblPartFullTime.Size = new System.Drawing.Size(99, 13);
-            this.lblPartFullTime.TabIndex = 31;
-            this.lblPartFullTime.Text = "Part Time/Full Time";
-            // 
-            // lblAddress2
-            // 
-            this.lblAddress2.AutoSize = true;
-            this.lblAddress2.Location = new System.Drawing.Point(252, 53);
-            this.lblAddress2.Name = "lblAddress2";
-            this.lblAddress2.Size = new System.Drawing.Size(51, 13);
-            this.lblAddress2.TabIndex = 30;
-            this.lblAddress2.Text = "Address2";
-            // 
             // lblAddress1
             // 
             this.lblAddress1.AutoSize = true;
-            this.lblAddress1.Location = new System.Drawing.Point(249, 27);
+            this.lblAddress1.Location = new System.Drawing.Point(242, 124);
             this.lblAddress1.Name = "lblAddress1";
-            this.lblAddress1.Size = new System.Drawing.Size(54, 13);
+            this.lblAddress1.Size = new System.Drawing.Size(45, 13);
             this.lblAddress1.TabIndex = 29;
-            this.lblAddress1.Text = "Address 1";
-            // 
-            // lblCell
-            // 
-            this.lblCell.AutoSize = true;
-            this.lblCell.Location = new System.Drawing.Point(29, 157);
-            this.lblCell.Name = "lblCell";
-            this.lblCell.Size = new System.Drawing.Size(34, 13);
-            this.lblCell.TabIndex = 28;
-            this.lblCell.Text = "Cell #";
+            this.lblAddress1.Text = "Address";
             // 
             // lblPhone
             // 
             this.lblPhone.AutoSize = true;
-            this.lblPhone.Location = new System.Drawing.Point(15, 131);
+            this.lblPhone.Location = new System.Drawing.Point(16, 208);
             this.lblPhone.Name = "lblPhone";
             this.lblPhone.Size = new System.Drawing.Size(48, 13);
             this.lblPhone.TabIndex = 27;
@@ -206,7 +152,7 @@
             // lblPosition
             // 
             this.lblPosition.AutoSize = true;
-            this.lblPosition.Location = new System.Drawing.Point(19, 105);
+            this.lblPosition.Location = new System.Drawing.Point(20, 182);
             this.lblPosition.Name = "lblPosition";
             this.lblPosition.Size = new System.Drawing.Size(44, 13);
             this.lblPosition.TabIndex = 26;
@@ -215,7 +161,7 @@
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(5, 79);
+            this.lblLastName.Location = new System.Drawing.Point(6, 156);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.Size = new System.Drawing.Size(58, 13);
             this.lblLastName.TabIndex = 25;
@@ -224,7 +170,7 @@
             // lblFirstName
             // 
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(6, 50);
+            this.lblFirstName.Location = new System.Drawing.Point(7, 127);
             this.lblFirstName.Name = "lblFirstName";
             this.lblFirstName.Size = new System.Drawing.Size(57, 13);
             this.lblFirstName.TabIndex = 24;
@@ -233,7 +179,7 @@
             // lblID
             // 
             this.lblID.AutoSize = true;
-            this.lblID.Location = new System.Drawing.Point(36, 27);
+            this.lblID.Location = new System.Drawing.Point(37, 72);
             this.lblID.Name = "lblID";
             this.lblID.Size = new System.Drawing.Size(18, 13);
             this.lblID.TabIndex = 23;
@@ -241,43 +187,112 @@
             // 
             // btnTerminate
             // 
-            this.btnTerminate.Location = new System.Drawing.Point(228, 191);
+            this.btnTerminate.Location = new System.Drawing.Point(226, 256);
             this.btnTerminate.Name = "btnTerminate";
             this.btnTerminate.Size = new System.Drawing.Size(75, 23);
             this.btnTerminate.TabIndex = 46;
             this.btnTerminate.Text = "Terminate";
             this.btnTerminate.UseVisualStyleBackColor = true;
             // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEmployeeID.Location = new System.Drawing.Point(70, 69);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(100, 20);
+            this.lblEmployeeID.TabIndex = 47;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "Role";
+            // 
+            // cbRole
+            // 
+            this.cbRole.FormattingEnabled = true;
+            this.cbRole.Location = new System.Drawing.Point(293, 176);
+            this.cbRole.Name = "cbRole";
+            this.cbRole.Size = new System.Drawing.Size(100, 21);
+            this.cbRole.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Country";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(293, 69);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 51;
+            this.txtCountry.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSIN
+            // 
+            this.txtSIN.Location = new System.Drawing.Point(70, 95);
+            this.txtSIN.Name = "txtSIN";
+            this.txtSIN.Size = new System.Drawing.Size(100, 20);
+            this.txtSIN.TabIndex = 52;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "SIN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(125, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 16);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "View and Edit Employee";
+            // 
             // ViewEditEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSIN);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbRole);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEmployeeID);
             this.Controls.Add(this.btnTerminate);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.txtPostalCode);
             this.Controls.Add(this.txtProvince);
-            this.Controls.Add(this.txtPartFullTime);
-            this.Controls.Add(this.txtAddress2);
-            this.Controls.Add(this.txtAddress1);
-            this.Controls.Add(this.txtCell);
+            this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.txtPosition);
             this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.txtFirstName);
-            this.Controls.Add(this.txtID);
             this.Controls.Add(this.lblPostalCode);
             this.Controls.Add(this.lblProvince);
-            this.Controls.Add(this.lblPartFullTime);
-            this.Controls.Add(this.lblAddress2);
             this.Controls.Add(this.lblAddress1);
-            this.Controls.Add(this.lblCell);
             this.Controls.Add(this.lblPhone);
             this.Controls.Add(this.lblPosition);
             this.Controls.Add(this.lblLastName);
             this.Controls.Add(this.lblFirstName);
             this.Controls.Add(this.lblID);
             this.Name = "ViewEditEmployee";
-            this.Size = new System.Drawing.Size(433, 227);
+            this.Size = new System.Drawing.Size(418, 296);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -288,26 +303,27 @@
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.TextBox txtPostalCode;
         private System.Windows.Forms.TextBox txtProvince;
-        private System.Windows.Forms.TextBox txtPartFullTime;
-        private System.Windows.Forms.TextBox txtAddress2;
-        private System.Windows.Forms.TextBox txtAddress1;
-        private System.Windows.Forms.TextBox txtCell;
+        private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.TextBox txtPhone;
         private System.Windows.Forms.TextBox txtPosition;
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.TextBox txtFirstName;
-        private System.Windows.Forms.TextBox txtID;
         private System.Windows.Forms.Label lblPostalCode;
         private System.Windows.Forms.Label lblProvince;
-        private System.Windows.Forms.Label lblPartFullTime;
-        private System.Windows.Forms.Label lblAddress2;
         private System.Windows.Forms.Label lblAddress1;
-        private System.Windows.Forms.Label lblCell;
         private System.Windows.Forms.Label lblPhone;
         private System.Windows.Forms.Label lblPosition;
         private System.Windows.Forms.Label lblLastName;
         private System.Windows.Forms.Label lblFirstName;
         private System.Windows.Forms.Label lblID;
         private System.Windows.Forms.Button btnTerminate;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbRole;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.TextBox txtSIN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- NerdBlock/Engine/Frontend/Winforms/Views/ViewEditEmployee.cs
+++ NerdBlock/Engine/Frontend/Winforms/Views/ViewEditEmployee.cs
@@ -16,5 +16,10 @@ namespace NerdBlock.Engine.Frontend.Winforms.Views
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
