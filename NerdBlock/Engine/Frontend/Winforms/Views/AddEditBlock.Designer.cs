cce07ec29380e81c6f248684f93fde745ac6a1ad
Alex C. Donnelly <alexandercassius@hotmail.com>
2016-11-28 13:32:40 -0500

    Made changes in AddEditBlock tools

--- NerdBlock/Engine/Frontend/Winforms/Views/AddEditBlock.Designer.cs
+++ NerdBlock/Engine/Frontend/Winforms/Views/AddEditBlock.Designer.cs
@@ -29,23 +29,21 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.grpBlockSummary = new System.Windows.Forms.GroupBox();
-            this.txtBlockTitle = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.lblCustCost = new System.Windows.Forms.Label();
+            this.lblOurCost = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lstItems = new System.Windows.Forms.ListBox();
             this.btnNew = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.lblOurCost = new System.Windows.Forms.Label();
-            this.lblCustCost = new System.Windows.Forms.Label();
-            this.lstItems = new System.Windows.Forms.ListBox();
             this.btnDelete = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            this.lblID = new System.Windows.Forms.Label();
-            this.txtDescription = new System.Windows.Forms.RichTextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.cbSeries = new System.Windows.Forms.ComboBox();
             this.grpBlockSummary.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -54,27 +52,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(7, 30);
+            this.label1.Location = new System.Drawing.Point(7, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Series";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Date";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(10, 152);
+            this.label3.Location = new System.Drawing.Point(10, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(151, 20);
             this.label3.TabIndex = 2;
@@ -84,7 +72,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(10, 182);
+            this.label4.Location = new System.Drawing.Point(10, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(151, 20);
             this.label4.TabIndex = 3;
@@ -92,32 +80,58 @@
             // 
             // grpBlockSummary
             // 
+            this.grpBlockSummary.Controls.Add(this.cbSeries);
+            this.grpBlockSummary.Controls.Add(this.txtTitle);
+            this.grpBlockSummary.Controls.Add(this.label5);
             this.grpBlockSummary.Controls.Add(this.label6);
             this.grpBlockSummary.Controls.Add(this.txtDescription);
-            this.grpBlockSummary.Controls.Add(this.lblID);
-            this.grpBlockSummary.Controls.Add(this.label5);
             this.grpBlockSummary.Controls.Add(this.lblCustCost);
             this.grpBlockSummary.Controls.Add(this.lblOurCost);
             this.grpBlockSummary.Controls.Add(this.label3);
             this.grpBlockSummary.Controls.Add(this.label4);
-            this.grpBlockSummary.Controls.Add(this.dtpDate);
             this.grpBlockSummary.Controls.Add(this.label1);
-            this.grpBlockSummary.Controls.Add(this.label2);
-            this.grpBlockSummary.Controls.Add(this.txtBlockTitle);
             this.grpBlockSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpBlockSummary.Location = new System.Drawing.Point(3, 389);
             this.grpBlockSummary.Name = "grpBlockSummary";
-            this.grpBlockSummary.Size = new System.Drawing.Size(720, 217);
+            this.grpBlockSummary.Size = new System.Drawing.Size(720, 166);
             this.grpBlockSummary.TabIndex = 4;
             this.grpBlockSummary.TabStop = false;
             this.grpBlockSummary.Text = "Block Summary";
             // 
-            // txtBlockTitle
+            // label6
             // 
-            this.txtBlockTitle.Location = new System.Drawing.Point(115, 33);
-            this.txtBlockTitle.Name = "txtBlockTitle";
-            this.txtBlockTitle.Size = new System.Drawing.Size(206, 27);
-            this.txtBlockTitle.TabIndex = 5;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(327, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(327, 51);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(386, 103);
+            this.txtDescription.TabIndex = 13;
+            this.txtDescription.Text = "";
+            // 
+            // lblCustCost
+            // 
+            this.lblCustCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCustCost.Location = new System.Drawing.Point(167, 134);
+            this.lblCustCost.Name = "lblCustCost";
+            this.lblCustCost.Size = new System.Drawing.Size(153, 20);
+            this.lblCustCost.TabIndex = 5;
+            // 
+            // lblOurCost
+            // 
+            this.lblOurCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOurCost.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblOurCost.Location = new System.Drawing.Point(167, 104);
+            this.lblOurCost.Name = "lblOurCost";
+            this.lblOurCost.Size = new System.Drawing.Size(153, 20);
+            this.lblOurCost.TabIndex = 4;
             // 
             // groupBox2
             // 
@@ -130,15 +144,14 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Items";
             // 
-            // dtpDate
+            // lstItems
             // 
-            this.dtpDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpDate.CustomFormat = "MM/yyyy";
-            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpDate.Location = new System.Drawing.Point(115, 69);
-            this.dtpDate.Name = "dtpDate";
-            this.dtpDate.Size = new System.Drawing.Size(206, 27);
-            this.dtpDate.TabIndex = 7;
+            this.lstItems.FormattingEnabled = true;
+            this.lstItems.ItemHeight = 20;
+            this.lstItems.Location = new System.Drawing.Point(6, 26);
+            this.lstItems.Name = "lstItems";
+            this.lstItems.Size = new System.Drawing.Size(699, 284);
+            this.lstItems.TabIndex = 0;
             // 
             // btnNew
             // 
@@ -158,32 +171,6 @@
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             // 
-            // lblOurCost
-            // 
-            this.lblOurCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblOurCost.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblOurCost.Location = new System.Drawing.Point(167, 152);
-            this.lblOurCost.Name = "lblOurCost";
-            this.lblOurCost.Size = new System.Drawing.Size(132, 20);
-            this.lblOurCost.TabIndex = 4;
-            // 
-            // lblCustCost
-            // 
-            this.lblCustCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblCustCost.Location = new System.Drawing.Point(167, 182);
-            this.lblCustCost.Name = "lblCustCost";
-            this.lblCustCost.Size = new System.Drawing.Size(132, 20);
-            this.lblCustCost.TabIndex = 5;
-            // 
-            // lstItems
-            // 
-            this.lstItems.FormattingEnabled = true;
-            this.lstItems.ItemHeight = 20;
-            this.lstItems.Location = new System.Drawing.Point(6, 26);
-            this.lstItems.Name = "lstItems";
-            this.lstItems.Size = new System.Drawing.Size(699, 284);
-            this.lstItems.TabIndex = 0;
-            // 
             // btnDelete
             // 
             this.btnDelete.Location = new System.Drawing.Point(558, 9);
@@ -197,37 +184,26 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(9, 104);
+            this.label5.Location = new System.Drawing.Point(6, 28);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 25);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "ID";
+            this.label5.Size = new System.Drawing.Size(49, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Title";
             // 
-            // lblID
+            // txtTitle
             // 
-            this.lblID.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblID.Location = new System.Drawing.Point(115, 106);
-            this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(117, 23);
-            this.lblID.TabIndex = 12;
+            this.txtTitle.Location = new System.Drawing.Point(115, 26);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(206, 27);
+            this.txtTitle.TabIndex = 16;
             // 
-            // txtDescription
+            // cbSeries
             // 
-            this.txtDescription.Location = new System.Drawing.Point(327, 51);
-            this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(386, 151);
-            this.txtDescription.TabIndex = 13;
-            this.txtDescription.Text = "";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(327, 23);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(109, 25);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "Description";
+            this.cbSeries.FormattingEnabled = true;
+            this.cbSeries.Location = new System.Drawing.Point(115, 63);
+            this.cbSeries.Name = "cbSeries";
+            this.cbSeries.Size = new System.Drawing.Size(205, 28);
+            this.cbSeries.TabIndex = 1;
             // 
             // AddEditBlock
             // 
@@ -239,7 +215,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.grpBlockSummary);
             this.Name = "AddEditBlock";
-            this.Size = new System.Drawing.Size(731, 609);
+            this.Size = new System.Drawing.Size(731, 566);
             this.Load += new System.EventHandler(this.AddEditBlock_Load);
             this.grpBlockSummary.ResumeLayout(false);
             this.grpBlockSummary.PerformLayout();
@@ -251,13 +227,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox grpBlockSummary;
-        private System.Windows.Forms.TextBox txtBlockTitle;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.DateTimePicker dtpDate;
         private System.Windows.Forms.Button btnNew;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Label lblCustCost;
@@ -265,8 +238,9 @@
         private System.Windows.Forms.ListBox lstItems;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.RichTextBox txtDescription;
-        private System.Windows.Forms.Label lblID;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbSeries;
     }
 }
