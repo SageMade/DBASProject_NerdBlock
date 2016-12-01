92e0e623c63d6480f75c81652e33d7633f43bd24
CASSlUS <alexandercassius@hotmail.com>
2016-11-30 20:38:56 -0500

    BlockTheme Renamed to BlockGenres
    Conforms to naming standard, controls for form changed.

--- NerdBlock/Engine/Frontend/Winforms/Views/BlockGenres.Designer.cs
+++ NerdBlock/Engine/Frontend/Winforms/Views/BlockGenres.Designer.cs
@@ -32,15 +32,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.txtTitle = new System.Windows.Forms.TextBox();
-            this.txtDescription = new System.Windows.Forms.TextBox();
-            this.chbInitialActive = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chbInitialActive = new System.Windows.Forms.CheckBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnViewSeries = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
@@ -48,27 +47,29 @@
             // lstSeries
             // 
             this.lstSeries.FormattingEnabled = true;
-            this.lstSeries.ItemHeight = 16;
-            this.lstSeries.Location = new System.Drawing.Point(16, 58);
+            this.lstSeries.Location = new System.Drawing.Point(12, 47);
+            this.lstSeries.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lstSeries.Name = "lstSeries";
-            this.lstSeries.Size = new System.Drawing.Size(314, 164);
+            this.lstSeries.Size = new System.Drawing.Size(236, 134);
             this.lstSeries.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.Size = new System.Drawing.Size(105, 20);
             this.label1.TabIndex = 9;
             this.label1.Text = "Block Genres";
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(234, 146);
+            this.btnAdd.Location = new System.Drawing.Point(176, 119);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(95, 23);
+            this.btnAdd.Size = new System.Drawing.Size(71, 19);
             this.btnAdd.TabIndex = 10;
             this.btnAdd.Text = "Add Genre";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -77,12 +78,23 @@
             // 
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.lstSeries);
-            this.groupBox1.Location = new System.Drawing.Point(19, 54);
+            this.groupBox1.Location = new System.Drawing.Point(14, 44);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(349, 242);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(262, 197);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "View Genres";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(23, 21);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Genre Name  |  Is Active?  |  # Of Series";
             // 
             // groupBox3
             // 
@@ -92,94 +104,83 @@
             this.groupBox3.Controls.Add(this.txtDescription);
             this.groupBox3.Controls.Add(this.txtTitle);
             this.groupBox3.Controls.Add(this.btnAdd);
-            this.groupBox3.Location = new System.Drawing.Point(19, 353);
+            this.groupBox3.Location = new System.Drawing.Point(14, 287);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(349, 182);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(262, 148);
             this.groupBox3.TabIndex = 13;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "New Genre";
             // 
-            // txtTitle
+            // label3
             // 
-            this.txtTitle.Location = new System.Drawing.Point(101, 37);
-            this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(100, 22);
-            this.txtTitle.TabIndex = 11;
-            // 
-            // txtDescription
-            // 
-            this.txtDescription.Location = new System.Drawing.Point(101, 76);
-            this.txtDescription.Multiline = true;
-            this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(228, 64);
-            this.txtDescription.TabIndex = 12;
-            // 
-            // chbInitialActive
-            // 
-            this.chbInitialActive.AutoSize = true;
-            this.chbInitialActive.Location = new System.Drawing.Point(207, 36);
-            this.chbInitialActive.Name = "chbInitialActive";
-            this.chbInitialActive.Size = new System.Drawing.Size(122, 21);
-            this.chbInitialActive.TabIndex = 13;
-            this.chbInitialActive.Text = "Initially Active?";
-            this.chbInitialActive.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Description";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 37);
+            this.label2.Location = new System.Drawing.Point(38, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.Size = new System.Drawing.Size(27, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "Title";
             // 
-            // label3
+            // chbInitialActive
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 76);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "Description";
+            this.chbInitialActive.AutoSize = true;
+            this.chbInitialActive.Location = new System.Drawing.Point(155, 32);
+            this.chbInitialActive.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chbInitialActive.Name = "chbInitialActive";
+            this.chbInitialActive.Size = new System.Drawing.Size(96, 17);
+            this.chbInitialActive.TabIndex = 13;
+            this.chbInitialActive.Text = "Initially Active?";
+            this.chbInitialActive.UseVisualStyleBackColor = true;
             // 
-            // label4
+            // txtDescription
             // 
-            this.label4.Location = new System.Drawing.Point(31, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(281, 23);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Genre Name  |  Is Active?  |  # Of Series";
+            this.txtDescription.Location = new System.Drawing.Point(69, 62);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(172, 53);
+            this.txtDescription.TabIndex = 12;
             // 
-            // button1
+            // txtTitle
             // 
-            this.button1.Location = new System.Drawing.Point(274, 314);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtTitle.Location = new System.Drawing.Point(69, 30);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(76, 20);
+            this.txtTitle.TabIndex = 11;
             // 
-            // button2
+            // btnViewSeries
             // 
-            this.button2.Location = new System.Drawing.Point(35, 314);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 23);
-            this.button2.TabIndex = 15;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnViewSeries.Location = new System.Drawing.Point(169, 255);
+            this.btnViewSeries.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnViewSeries.Name = "btnViewSeries";
+            this.btnViewSeries.Size = new System.Drawing.Size(106, 19);
+            this.btnViewSeries.TabIndex = 14;
+            this.btnViewSeries.Text = "View Series";
+            this.btnViewSeries.UseVisualStyleBackColor = true;
             // 
             // BlockGenres
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnViewSeries);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "BlockGenres";
-            this.Size = new System.Drawing.Size(385, 550);
+            this.Size = new System.Drawing.Size(289, 447);
             this.groupBox1.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
@@ -201,7 +202,6 @@
         private System.Windows.Forms.CheckBox chbInitialActive;
         private System.Windows.Forms.TextBox txtDescription;
         private System.Windows.Forms.TextBox txtTitle;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnViewSeries;
     }
 }
--- NerdBlock/Engine/Frontend/Winforms/Views/BoxesSoldPerTheme.Designer.cs
+++ NerdBlock/Engine/Frontend/Winforms/Views/BoxesSoldPerTheme.Designer.cs
@@ -40,40 +40,35 @@
             // lstBoxContent
             // 
             this.lstBoxContent.FormattingEnabled = true;
-            this.lstBoxContent.ItemHeight = 16;
-            this.lstBoxContent.Location = new System.Drawing.Point(43, 75);
-            this.lstBoxContent.Margin = new System.Windows.Forms.Padding(4);
+            this.lstBoxContent.Location = new System.Drawing.Point(32, 61);
             this.lstBoxContent.Name = "lstBoxContent";
-            this.lstBoxContent.Size = new System.Drawing.Size(403, 340);
+            this.lstBoxContent.Size = new System.Drawing.Size(303, 277);
             this.lstBoxContent.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(32, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Theme";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(117, 52);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(88, 42);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Quantity Subbed";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(370, 52);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(278, 42);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.Size = new System.Drawing.Size(58, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Total Price";
             // 
@@ -81,34 +76,35 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(41, 14);
+            this.label5.Location = new System.Drawing.Point(31, 11);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(208, 25);
+            this.label5.Size = new System.Drawing.Size(158, 20);
             this.label5.TabIndex = 5;
-            this.label5.Text = "Boxes Sold by Genere";
+            this.label5.Text = "Boxes Sold by Genre";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // cbTheme
             // 
             this.cbTheme.FormattingEnabled = true;
-            this.cbTheme.Location = new System.Drawing.Point(311, 14);
+            this.cbTheme.Location = new System.Drawing.Point(233, 11);
+            this.cbTheme.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbTheme.Name = "cbTheme";
-            this.cbTheme.Size = new System.Drawing.Size(135, 24);
+            this.cbTheme.Size = new System.Drawing.Size(102, 21);
             this.cbTheme.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(248, 52);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(186, 42);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.Size = new System.Drawing.Size(86, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Quantity Subbed";
             // 
             // BoxesSoldPerTheme
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cbTheme);
@@ -117,9 +113,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lstBoxContent);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "BoxesSoldPerTheme";
-            this.Size = new System.Drawing.Size(491, 462);
+            this.Size = new System.Drawing.Size(368, 375);
             this.ResumeLayout(false);
             this.PerformLayout();
 
