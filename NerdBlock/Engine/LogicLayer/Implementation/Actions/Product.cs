e95b9bac10c24992ae5d404d64bb117fa6491c92
Daniel Munusami <daniel.munusami@gmail.com>
2016-11-30 21:19:36 -0500

    Products

---
+++ NerdBlock/Engine/LogicLayer/Implementation/Actions/Product.cs
@@ -0,0 +1,21 @@
+﻿using NerdBlock.Engine.Backend;
+using NerdBlock.Engine.Backend.Models;
+using NerdBlock.Engine.Frontend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdBlock.Engine.LogicLayer.Implementation.Actions
+{
+    [BusinessActionContainer]
+    public class Product
+    {
+        [BusinessAction("goto_inventory_search")]
+        public void ShowSearch()
+        {
+            ViewManager.Show("Inventory")
+        }
+    }
+}
