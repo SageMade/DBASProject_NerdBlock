92c3e787f8ba2da5a433a75fee5ec752a8c73947
Daniel Munusami <daniel.munusami@gmail.com>
2016-12-06 21:05:16 -0500

    AddOrder

--- NerdBlock/Engine/Frontend/Winforms/Views/AddOrder.cs
+++ NerdBlock/Engine/Frontend/Winforms/Views/AddOrder.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NerdBlock.Engine.Frontend.Winforms.Views;
+using NerdBlock.Engine.Backend.Models;
+using NerdBlock.Engine.Backend;
+using NerdBlock.Engine.LogicLayer;
+using NerdBlock.Engine.Frontend.Winforms.Implementation;
 
 //Fields that do not need tools
 //Orderid - Auto generated
@@ -19,6 +24,15 @@ namespace NerdBlock.Engine.Frontend.Winforms.Views
         public AddOrder()
         {
             InitializeComponent();
+
+            Inputs.Add(new TextBoxInput("QuantityOrdered", txtQuantityOrdered));
+
+            ViewManager.PopulateFromQuery(cbProductID, DataAccess.Execute("select productid from tblproduct group by productid order by productid"));
+            ViewManager.PopulateFromQuery(cbSupplierID, DataAccess.Execute("select supplierid from tblsupplier group by supplierid order by supplierid"));
+
+            btnAddOrder.Click += (X, Y) => AttemptAction("insert_order");
+            btnAddProduct.Click += (X, Y) => AttemptAction("goto_inventory_add");
+            btnAddSupplier.Click += (X, Y) => AttemptAction("goto_supplier_add");
         }
 
         private void txtDateArrived_TextChanged(object sender, EventArgs e)
