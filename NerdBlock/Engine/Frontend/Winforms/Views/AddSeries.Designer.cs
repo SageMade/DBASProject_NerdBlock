fa1ca5913d2fe0f7bd60f606bfccfa5d48c95e95
ShawnM427 <me@shawnm.ca>
2016-12-08 01:11:31 -0500

    Start on AddSeries page
    Can't beleive we forgot this

---
+++ NerdBlock/Engine/Frontend/Winforms/Views/AddSeries.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NerdBlock.Engine.Frontend.Winforms.Views
+{
+    partial class AddSeries
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtSeriesName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.cbGenre = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Add Block Seies";
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(130, 87);
+            this.txtVolume.Margin = new System.Windows.Forms.Padding(2);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(84, 20);
+            this.txtVolume.TabIndex = 49;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(130, 63);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(84, 20);
+            this.txtPrice.TabIndex = 48;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(36, 90);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(89, 13);
+            this.lblVolume.TabIndex = 45;
+            this.lblVolume.Text = "Box Volume (cm³)";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(63, 70);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(62, 13);
+            this.lblPrice.TabIndex = 44;
+            this.lblPrice.Text = "Price (CAD)";
+            // 
+            // txtSeriesName
+            // 
+            this.txtSeriesName.Location = new System.Drawing.Point(130, 41);
+            this.txtSeriesName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSeriesName.Name = "txtSeriesName";
+            this.txtSeriesName.Size = new System.Drawing.Size(190, 20);
+            this.txtSeriesName.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Series Name";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(131, 138);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 28);
+            this.btnAdd.TabIndex = 52;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Location = new System.Drawing.Point(89, 115);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(36, 13);
+            this.lblGenre.TabIndex = 53;
+            this.lblGenre.Text = "Genre";
+            // 
+            // cbGenre
+            // 
+            this.cbGenre.FormattingEnabled = true;
+            this.cbGenre.Location = new System.Drawing.Point(131, 112);
+            this.cbGenre.Name = "cbGenre";
+            this.cbGenre.Size = new System.Drawing.Size(189, 21);
+            this.cbGenre.TabIndex = 54;
+            // 
+            // AddSeries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cbGenre);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.txtSeriesName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "AddSeries";
+            this.Size = new System.Drawing.Size(338, 175);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtSeriesName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.ComboBox cbGenre;
+    }
+}
---
+++ NerdBlock/Engine/Frontend/Winforms/Views/AddSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NerdBlock.Engine.Frontend.Winforms.Views;
+using NerdBlock.Engine.Backend.Models;
+using NerdBlock.Engine.Backend;
+using NerdBlock.Engine.LogicLayer;
+using NerdBlock.Engine.Frontend.Winforms.Implementation;
+
+namespace NerdBlock.Engine.Frontend.Winforms.Views
+{
+    public partial class AddSeries : ViewBase
+    {
+        public AddSeries()
+        {
+            InitializeComponent();
+            
+            Inputs.Add(new TextBoxInput("Name", txtSeriesName));
+            Inputs.Add(new TextBoxInput("Price", txtPrice));
+            Inputs.Add(new TextBoxInput("Volume", txtVolume));
+
+            btnAdd.Click += (X, Y) => AttemptAction("insert_series");
+        }
+    }
+}
